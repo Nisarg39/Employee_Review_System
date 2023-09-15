a929980c7e9959515197fe5d95ac54dda9da0d22
Nisarg Shah <shah.nisarg39@gmail.com>
2023-09-15 14:43:44 +0530

    added memorystore for multiple usage

--- index.js
+++ index.js
@@ -5,20 +5,25 @@ const port = process.env.PORT || 8000;
 require('dotenv').config({ override: true })
 var bodyParser = require('body-parser');
 const expressSession = require('express-session')
+const MemoryStore = require('memorystore')(expressSession)
 
 const passport= require('passport');        //using passport.js for authentication
 const {initializingPassport} = require('./config/passport-local-strategy');
 
 const db = require('./config/mongoose');
 
+app.use(expressSession({store: new MemoryStore({
+    checkPeriod: 100000 // prune expired entries every 24h
+  }),      
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false}
+));
+
 initializingPassport(passport);
 
 
-app.use(expressSession({        //always use express session before passport initilization
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: false
-}));
+
 
 app.use(passport.initialize());
 app.use(passport.session());
